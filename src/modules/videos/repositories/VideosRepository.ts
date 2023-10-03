f2008af48b4c61c181312a16ed26d62105795608
Guilherme Marques <guilhermeprogramacao2003@gmail.com>
2023-10-03 19:31:43 -0300

    first commit

---
+++ src/modules/videos/repositories/VideosRepository.ts
@@ -0,0 +1,62 @@
+import { pool } from '../../../mysql';
+import { v4 as uuidv4 } from 'uuid';
+import { hash, compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken'
+import { Request, Response } from 'express';
+
+class VideosRepository {
+    create(request: Request, response: Response) {
+        const { user_id, title, description, thumbnail, publishedAt } = request.body;
+        pool.getConnection((error: any, connection: any) => {
+            connection.query(
+                'INSERT INTO videos (video_id, user_id, title, description, thumbnail, publishedAt) VALUES (?,?,?,?,?,?)',
+                [uuidv4(), user_id, title, description, thumbnail, publishedAt],
+                (error: any, result: any, fileds: any) => {
+                    connection.release();
+                    if (error) {
+                        return response.status(400).json(error)
+                    }
+                    response.status(200).json({ message: 'Video criado com sucesso' });
+                }
+            )
+        })
+    }
+
+    searchVideos(request: Request, response: Response){
+        const { search } = request.query;
+        pool.getConnection((error: any, connection: any) => {
+
+            connection.query(
+                'SELECT * FROM videos WHERE title LIKE ?',
+                [`%${search}%`],
+                (error: any, results: any, fileds: any) => {
+                    connection.release();
+                    if (error) {
+                        return response.status(400).json({ error: 'Erro ao buscar os videos!' })
+                    }
+                    return response.status(200).json({ message:'Videos retornados com sucesso', videos: results })
+                }
+            )
+        })
+    }
+
+    getVideos(request: Request, response: Response){
+        const { user_id } = request.query;
+        pool.getConnection((error: any, connection: any) => {
+
+            connection.query(
+                'SELECT * FROM videos WHERE user_id = ?',
+                [user_id],
+                (error: any, results: any, fileds: any) => {
+                    connection.release();
+                    if (error) {
+                        return response.status(400).json({ error: 'Erro ao buscar os videos!' })
+                    }
+                    return response.status(200).json({ message:'Videos retornados com sucesso', videos: results })
+                }
+            )
+        })
+    }
+}
+
+export { VideosRepository }
---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express, { request, response } from 'express'
+import { userRoutes } from './routes/user.routes';
+import { videosRoutes } from './routes/videos.routes';
+import { config } from 'dotenv';
+
+config();
+const app = express();
+const port = process.env.PORT || 9001;
+
+const cors = require('cors');
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, OPTIONS");
+    next();
+})
+
+app.use(cors());
+
+app.use(express.json());
+app.use('/user', userRoutes);
+app.use('/videos', videosRoutes);
+
+
+
+app.listen(port, () => {
+    console.log(`Serve started ${port}`);
+  });
